0414b96fd0ea9770ad21a60c5a072f3d85cd7653
Mahmoud Kamal <mk.bayumi@gmail.com>
2024-06-18 23:25:29 +0300

    refining styles

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -5,12 +5,8 @@ export default function Help() {
     <>
       <section className="help-header">
         <div className="container">
-          <h2 className='main-title'>Help</h2>
+          <h2 className='page-title'>Help</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam neque delectus sapiente corrupti, suscipit repudiandae nobis earum cupiditate officiis itaque inventore assumenda libero quis, quae adipisci quibusdam rem illum voluptas.</p>
-        </div>
-      </section>
-      <section className="help-nav">
-        <div className="container">
           <div className="help-options">
             <NavLink to="faq">FAQ</NavLink>
             <NavLink to="contact">Contect Us</NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
-import { Routes, Route } from 'react-router-dom'
-
-import About from "./About"
-
 export default function Home() {
   return (
     <section className="Home">
       <div className="container">
-        <h2 className='main-title'>Welcome</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam neque delectus sapiente corrupti, suscipit repudiandae nobis earum cupiditate officiis itaque inventore assumenda libero quis, quae adipisci quibusdam rem illum voluptas.</p>
+        <h2 className='page-title'>Welcome</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adsipisicing elit. Ullam neque delectus sapiente corrupti, suscipit repudiandae nobis earum cupiditate officiis itaque inventore assumenda libero quis, quae adipisci quibusdam rem illum voluptas.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam neque delectus sapiente corrupti, suscipit repudiandae nobis earum cupiditate officiis itaque inventore assumenda libero quis, quae adipisci quibusdam rem illum voluptas.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam neque delectus sapiente corrupti, suscipit repudiandae nobis earum cupiditate officiis itaque inventore assumenda libero quis, quae adipisci quibusdam rem illum voluptas.</p>
       </div>
